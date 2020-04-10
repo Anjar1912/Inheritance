b0d4fb8559fdb042a7e6f5604247d35ade6fdfff
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-04-10 10:06:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Anjar Tri Sasongko", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Anjar Tri Sasongko", 20, "19.11.2607", "anjar.1912@students.@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
